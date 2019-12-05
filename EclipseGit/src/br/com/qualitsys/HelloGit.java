121c9486e4ce8f38336dd4d0b13514a37f1b26e6
Aparecido <Aparecido@10.0.20.3>
2019-12-05 18:46:04 -0200

    Commit 3 - 17h47

--- EclipseGit/src/br/com/qualitsys/HelloGit.java
+++ EclipseGit/src/br/com/qualitsys/HelloGit.java
@@ -3,7 +3,8 @@ package br.com.qualitsys;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("***Hello Git, versao V2 ***");
+		System.out.println("***Hello Git, versao V3 ***");
+		System.out.println("Interface GIT com Eclipse....");
 	}
 
 }
