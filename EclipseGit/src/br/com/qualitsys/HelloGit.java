745dfea7b7196b3a41abf2f1185b118a81cf552c
Aparecido <Aparecido@10.0.20.3>
2019-12-05 18:39:43 -0200

    Commit, V2 - 17h50 - 5/12

--- EclipseGit/src/br/com/qualitsys/HelloGit.java
+++ EclipseGit/src/br/com/qualitsys/HelloGit.java
@@ -3,8 +3,7 @@ package br.com.qualitsys;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git, V1");
-
+		System.out.println("***Hello Git, versao V2 ***");
 	}
 
 }
