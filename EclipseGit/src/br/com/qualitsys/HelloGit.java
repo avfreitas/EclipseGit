f355af22bdd7e54808fb62047e2c43d71d3cf9e3
Aparecido <Aparecido@10.0.20.14>
2019-12-12 16:14:18 -0200

    Commit no dia da visita do CEE

--- EclipseGit/src/br/com/qualitsys/HelloGit.java
+++ EclipseGit/src/br/com/qualitsys/HelloGit.java
@@ -3,7 +3,8 @@ package br.com.qualitsys;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("***Hello Git, versao V3 ***");
+		System.out.println("***Hello Git, versao V4 ***");
+		System.out.println("No dia da visita do CEE - 12/12/2019 - 15h15");
 		System.out.println("Interface GIT com Eclipse....");
 	}
 
