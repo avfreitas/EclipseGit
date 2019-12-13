ff0278cf74836ab880f1cdabf2ea93f265b80bf1
Aparecido <Aparecido@HP>
2019-12-13 21:49:57 -0200

    Commit ----  13/12/2019 - 20h45 .....

--- EclipseGit/src/br/com/qualitsys/HelloGit.java
+++ EclipseGit/src/br/com/qualitsys/HelloGit.java
@@ -3,9 +3,10 @@ package br.com.qualitsys;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("***Hello Git, versao V7 ***");
+		System.out.println("***Hello Git, versao V8 ***");
 		System.out.println("Versão criada no dia 13/12/2019");
-		System.out.println("Hora:  20h45   .....");
+		System.out.println("Hora:  20h50.... .....");
+		System.out.println("Acho que agora estou dominando o GIT>>>>>>>");
 	}
-
+ 
 }
